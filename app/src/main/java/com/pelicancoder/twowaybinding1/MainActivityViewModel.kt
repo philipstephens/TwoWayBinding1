2300fe317b52ca324cacd018a443b6cf27629a7a
Philip Stephens <plusgforce@gmail.com>
2023-02-03 22:43:48 -0600

    Initial commit

---
+++ app/src/main/java/com/pelicancoder/twowaybinding1/MainActivityViewModel.kt
@@ -0,0 +1,12 @@
+package com.pelicancoder.twowaybinding1
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel : ViewModel() {
+    val userName = MutableLiveData<String>()
+
+    init {
+        userName.value = "Frank"
+    }
+}
